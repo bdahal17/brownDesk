d1fb7aeca71d1d2a4f44be613a9f7277e253d8d3
Bishnu Dahal <bishnudahal@Bishnus-MacBook-Pro.local>
2023-02-25 23:42:24 -0500

    initial

---
+++ src/main/java/com/example/browndesk/BrownDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.browndesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrownDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/browndesk/BrownDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.browndesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrownDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
